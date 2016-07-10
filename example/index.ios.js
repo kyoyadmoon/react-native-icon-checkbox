ec93e049bf56cc0eedff068aebb954aadf19d150
dmoon <dmoon.t@gmail.com>
2016-07-10 10:22:49 +0800

    add example && fix style proptype`

---
+++ example/index.ios.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Example from './Example';
+
+class example extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Example />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('example', () => example);
