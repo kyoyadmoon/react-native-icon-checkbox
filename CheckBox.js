cc5d17f0db6c8f38a6df891d64b43f3cef3277cf
Vojtech Novak <vonovak@gmail.com>
2016-08-12 22:39:08 +0200

    Update CheckBox.js

--- CheckBox.js
+++ CheckBox.js
@@ -1,8 +1,8 @@
-import React, {
+import {
   Text,
-  PropTypes,
   StyleSheet,
 } from 'react-native';
+import React, {PropTypes} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function CheckBox(props) {
